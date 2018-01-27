0e26970cee50c11a950070deaf6be773418762c1
Christopher Lowenthal <clowenthal@hybridgames.net>
2018-01-27 16:28:36 -0500

    removed processEmitter

--- src/shapes/stage.js
+++ src/shapes/stage.js
@@ -66,48 +66,6 @@ export default class Stage {
         return null;
     }
 
-    // processEmitter(ray) {
-    //     //Stretch ray to maximum length for line/line intersections
-    //     let vector = ray.vector.multiply(this.maxLength);
-    //     ray.end.x = ray.start.x + vector.x;
-    //     ray.end.y = ray.start.y + vector.y;
-
-    //     let minDistance = this.maxLength;
-    //     let distance;
-    //     let intersectionPoint;
-    //     let currentPoint = null;
-    //     let currentObj = null;
-
-    //     let objs = this.tools.concat(this.collectors);
-    //     debugger
-    //     objs.forEach((obj) => {
-    //         if (obj.intersect(ray) !== false) {
-    //             intersectionPoint = obj.intersectPoint(ray);
-    //             distance = intersectionPoint.distance(ray.start);
-    //             if (distance < minDistance && intersectionPoint.distance(ray.start) > Number.EPSILON) {
-    //                 minDistance = distance;
-    //                 currentPoint = intersectionPoint;
-    //                 currentObj = obj;
-    //             }
-    //         }
-    //     });
-
-    //     if (currentObj) {
-    //         let result = currentObj.cast(ray);
-    //         let extend = result.end.difference(result.start).multiply(this.maxLength);
-    //         result.end.x = result.start.x + extend.x;
-    //         result.end.y = result.start.y + extend.y;
-
-    //         if (result.intensity - Number.EPSILON > 0) {
-    //             this.processEmitter(result);
-    //         }
-    //         console.log(`Intensity: ${result.intensity}`);
-    //         this.lines.push(new Line(ray.start, intersectionPoint, ray.color, ray.width));
-    //     } else {
-    //         this.lines.push(ray);
-    //     }
-    // }
-
     simulate() {
         this.lines = [];
         let rays = [].concat(this.emitters);
