1b18d18c36ca521ec2c05672fa9ded4c5fe056bb
Christopher Lowenthal <clowenthal@hybridgames.net>
2018-01-27 03:49:51 -0500

    Added a simple Stage object

---
+++ shapes/stage.js
@@ -0,0 +1,42 @@
+export default class Stage {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.emitters = [];
+        this.collectors = [];
+        this.tools = [];
+        this.lines = [];
+        this.simulate();
+    }
+
+    add(tool) {
+        this.tools.push(tool);
+    }
+
+    simulate() {
+        this.lines = [
+            {
+                start: {
+                    x: 0,
+                    y: 0
+                },
+                end: {
+                    x: this.width,
+                    y: this.height
+                },
+                color: '#FFFFFF'
+            },
+            {
+                start: {
+                    x: 20,
+                    y: 40
+                },
+                end: {
+                    x: this.width / 2,
+                    y: this.height / 2
+                },
+                color: '#FF0000'
+            }
+        ]
+    }
+}
