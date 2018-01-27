43070360b74abe27618bd3bc547a5b254a33be14
Christopher Lowenthal <clowenthal@hybridgames.net>
2018-01-27 03:29:17 -0500

    Yea... this is some of the stuff started twice.

---
+++ shapes/prism.js
@@ -0,0 +1,21 @@
+export default class Prism {
+    cast(ray) {
+        return [
+            {
+                x: .4,
+                y: .6,
+                color: 'FF0000'
+            },
+            {
+                x: .5,
+                y: .5,
+                color: '00FF00'
+            },
+            {
+                x: .6,
+                y: .4,
+                color: '0000FF'
+            }
+        ]
+    }
+}
