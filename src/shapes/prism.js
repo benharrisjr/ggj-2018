e026254007aec4bd61a24b49e150d555240631c2
Benjamin Harris Jr <benharrisjr+sourcetree@gmail.com>
2018-01-27 19:35:35 -0500

    Prism stuffs

--- src/shapes/prism.js
+++ src/shapes/prism.js
@@ -1,21 +1,48 @@
+import Vector from './vector';
+import Line from './line';
+
 export default class Prism {
+    constructor(line0, line1, line3) {
+        this.line0 = line0;
+        this.line1 = line1;
+        this.line2 = line2;
+        this.loss = .1;
+        this.normal = this.line.vector.normal;
+    }
+
     cast(ray) {
-        return [
-            {
-                x: .4,
-                y: .6,
-                color: 'FF0000'
-            },
-            {
-                x: .5,
-                y: .5,
-                color: '00FF00'
-            },
-            {
-                x: .6,
-                y: .4,
-                color: '0000FF'
-            }
-        ]
+        let refRay = ray.end.difference(ray.start).normalized;
+        let dot = 2 * refRay.dot(this.normal)
+
+        let dx = refRay.x - dot * this.normal.x
+        let dy = refRay.y - dot * this.normal.y
+
+        let start = this.intersectPoint(ray);
+
+        let reverse = ray.start.difference(start).normalized;
+        start.x += reverse.x;
+        start.y += reverse.y;
+
+        let r1 = new Vector(start.x + dx, start.y + dy);
+
+        let result = [
+            new Line(start, r1, ray.color, ray.width, ray.intensity - this.loss),
+            new Line(start, r1, ray.color, ray.width, ray.intensity - this.loss),
+            new Line(start, r1, ray.color, ray.width, ray.intensity - this.loss),
+        ];
+
+        return result;
+    }
+
+    intersect(ray) {
+        return this.line0.intersects(ray);
+        return this.line1.intersects(ray);
+        return this.line2.intersects(ray);
+    }
+
+    intersectPoint(ray) {
+        return this.line0.intersectPoint(ray);
+        return this.line1.intersectPoint(ray);
+        return this.line2.intersectPoint(ray);
     }
 }
