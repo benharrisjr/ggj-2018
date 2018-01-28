2529e25fc3d48b70710127267e49400c1a8c88a3
Christopher Lowenthal <clowenthal@hybridgames.net>
2018-01-28 01:29:45 -0500

    Slit Level, and collector fill

--- src/levels/levels.js
+++ src/levels/levels.js
@@ -1,6 +1,9 @@
 import MirrorMaze from './mirror-maze';
+import Slit from './slit';
+
 const Levels = {
-    'Mirror Maze': MirrorMaze
+    'Mirror Maze': MirrorMaze,
+    'Slit': Slit
 }
 
 export default Levels;
---
+++ src/levels/slit.js
@@ -0,0 +1,23 @@
+import Line from "../shapes/line";
+import Vector from "../shapes/vector";
+import Blocker from "../shapes/blocker";
+import Rectangle from "../shapes/rectangle";
+import Collector from "../shapes/collector";
+import Circle from "../shapes/circle";
+
+const Slit = {
+    emitters: [
+        new Line(new Vector(400, 0), new Vector(400, 1), '#FFFFFF', 2, 1)
+    ],
+    blockers: [
+        new Blocker(new Rectangle(new Vector(100, 300), 300, 20, 0)),
+        new Blocker(new Rectangle(new Vector(700, 300), 300, 20, 0)),
+        new Blocker(new Rectangle(new Vector(400, 300), 200, 20, 0))
+    ],
+    collectors: [
+        new Collector(new Circle(new Vector(200, 500), 20)),
+        new Collector(new Circle(new Vector(600, 500), 20))
+    ]
+}
+
+export default Slit;
