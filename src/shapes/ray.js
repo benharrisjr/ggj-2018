cc07c8670a1cc16806df687c3e63bdd0bde0e951
Benjamin Harris Jr <benharrisjr+sourcetree@gmail.com>
2018-01-27 19:46:38 -0500

    Updates

--- src/shapes/ray.js
+++
@@ -1,8 +0,0 @@
-export default class Ray {
-    constructor(line, color, width, intesity) {
-        this.line = line;
-        this.color = color;
-        this.width = width;
-        this.intensity = intensity
-    }
-}
--- src/shapes/refraction.js
+++
@@ -1,9 +0,0 @@
-export default class Refraction {
-    constructor(index) {
-        this.index = index;
-    }
-
-    cast(ray) {
-
-    }
-}
